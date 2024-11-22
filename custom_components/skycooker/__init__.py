e00e5befb1247d711c690e2685c467470ed2fe6f
Maksim Kovalyov <kaizer@kai-zer.ru>
2024-11-23 00:40:03 +0800

    fix

--- custom_components/skycooker/__init__.py
+++ custom_components/skycooker/__init__.py
@@ -1,35 +1,13 @@
 """Support for SkyCooker."""
+
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (ATTR_SW_VERSION,
-                                 CONF_FRIENDLY_NAME, CONF_MAC,
-                                 Platform)
+from homeassistant.const import ATTR_SW_VERSION, CONF_FRIENDLY_NAME, CONF_MAC, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 
-
-DOMAIN = "skycoker"
-FRIENDLY_NAME = "SkyCooker"
-MANUFACTORER = "Redmond"
-SUGGESTED_AREA = "kitchen"
-
-CONF_PERSISTENT_CONNECTION = "persistent_connection"
-
-DEFAULT_SCAN_INTERVAL = 5
-DEFAULT_PERSISTENT_CONNECTION = True
-
-DATA_CONNECTION = "connection"
-DATA_CANCEL = "cancel"
-DATA_WORKING = "working"
-DATA_DEVICE_INFO = "device_info"
-
-DISPATCHER_UPDATE = "update"
-
-ROOM_TEMP = 25
-BOIL_TEMP = 100
-
-BLE_SCAN_TIME = 3
+from .const import *
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -46,9 +24,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up SkyCooker integration from a config entry."""
     entry.async_on_unload(entry.add_update_listener(entry_update_listener))
 
-    if DOMAIN not in hass.data: hass.data[DOMAIN] = {}
-    if entry.entry_id not in hass.data: hass.data[DOMAIN][entry.entry_id] = {}
-
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    if entry.entry_id not in hass.data:
+        hass.data[DOMAIN][entry.entry_id] = {}
 
     hass.data[DOMAIN][DATA_WORKING] = True
     hass.data[DOMAIN][DATA_DEVICE_INFO] = lambda: device_info(entry)
@@ -57,19 +36,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     return True
 
+
 def device_info(entry):
     return DeviceInfo(
         name=(FRIENDLY_NAME + " " + entry.data.get(CONF_FRIENDLY_NAME, "")).strip(),
         manufacturer=MANUFACTORER,
         model=entry.data.get(CONF_FRIENDLY_NAME, None),
         sw_version=entry.data.get(ATTR_SW_VERSION, None),
-        identifiers={
-            (DOMAIN, entry.data[CONF_MAC])
-        },
-        connections={
-            ("mac", entry.data[CONF_MAC])
-        }
+        identifiers={(DOMAIN, entry.data[CONF_MAC])},
+        connections={("mac", entry.data[CONF_MAC])},
     )
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
@@ -83,6 +60,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN][entry.entry_id][DATA_CONNECTION] = None
     _LOGGER.debug("Entry unloaded")
     return True
+
 
 async def entry_update_listener(hass, entry):
     """Handle options update."""
--- custom_components/skycooker/config_flow.py
+++ custom_components/skycooker/config_flow.py
@@ -1,20 +1,9 @@
 """Config flow for SkyCooker integration."""
 
-import logging
-import secrets
-import traceback
-
-import voluptuous as vol
-
-import homeassistant.helpers.config_validation as cv
 from homeassistant import config_entries
-from homeassistant.components import bluetooth
-from homeassistant.const import (CONF_FRIENDLY_NAME, CONF_MAC,
-                                 CONF_PASSWORD, CONF_SCAN_INTERVAL, Platform)
 from homeassistant.core import callback
+from .const import *
 
-
-DOMAIN = "skycoker"
 
 class SkyCookerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -25,7 +14,7 @@ class SkyCookerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Get options flow for this handler."""
         return SkyCookerConfigFlow(entry=entry)
 
-    def __init__(self, entry = None):
+    def __init__(self, entry=None):
         """Initialize a new SkyCookerConfigFlow."""
         self.entry = entry
         self.config = {} if not entry else dict(entry.data.items())
