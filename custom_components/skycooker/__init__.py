37371b6b276726ecdf9e895e4acb16d30ce0d79e
Maksim Kovalyov <kaizer@kai-zer.ru>
2024-11-23 01:18:12 +0800

    fix

--- custom_components/skycooker/__init__.py
+++ custom_components/skycooker/__init__.py
@@ -1,33 +1,66 @@
 """Support for SkyCooker."""
-
 import logging
+from datetime import timedelta
 
+import homeassistant.helpers.event as ev
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_SW_VERSION, CONF_FRIENDLY_NAME, CONF_MAC
+from homeassistant.const import (ATTR_SW_VERSION, CONF_DEVICE,
+                                 CONF_FRIENDLY_NAME, CONF_MAC, CONF_PASSWORD,
+                                 CONF_SCAN_INTERVAL, Platform)
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.dispatcher import dispatcher_send
 from homeassistant.helpers.entity import DeviceInfo
 
 from .const import *
+from .cooker_connection import CookerConnection
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORMS = [
+    Platform.WATER_HEATER,
+    Platform.SWITCH,
+    Platform.LIGHT,
+    Platform.SENSOR,
+    Platform.NUMBER,
+]
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up SkyCooker integration from a config entry."""
-    _LOGGER.info("async_setup_entry")
     entry.async_on_unload(entry.add_update_listener(entry_update_listener))
 
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
-    if entry.entry_id not in hass.data:
-        hass.data[DOMAIN][entry.entry_id] = {}
+    if DOMAIN not in hass.data: hass.data[DOMAIN] = {}
+    if entry.entry_id not in hass.data: hass.data[DOMAIN][entry.entry_id] = {}
+
+    cooker = CookerConnection(
+        mac=entry.data[CONF_MAC],
+        key=entry.data[CONF_PASSWORD],
+        persistent=entry.data[CONF_PERSISTENT_CONNECTION],
+        adapter=entry.data.get(CONF_DEVICE, None),
+        hass=hass,
+        model=entry.data.get(CONF_FRIENDLY_NAME, None)
+    )
+    hass.data[DOMAIN][entry.entry_id][DATA_CONNECTION] = cooker
+
+    async def poll(now, **kwargs) -> None:
+        await cooker.update()
+        await hass.async_add_executor_job(dispatcher_send, hass, DISPATCHER_UPDATE)
+        if hass.data[DOMAIN][DATA_WORKING]:
+            schedule_poll(timedelta(seconds=entry.data[CONF_SCAN_INTERVAL]))
+        else:
+            _LOGGER.info("Not working anymore, stop")
+
+    def schedule_poll(td):
+        hass.data[DOMAIN][DATA_CANCEL] = ev.async_call_later(hass, td, poll)
 
     hass.data[DOMAIN][DATA_WORKING] = True
     hass.data[DOMAIN][DATA_DEVICE_INFO] = lambda: device_info(entry)
 
-    await hass.config_entries.async_forward_entry_setups(entry, [])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    schedule_poll(timedelta(seconds=3))
 
     return True
-
 
 def device_info(entry):
     return DeviceInfo(
@@ -35,18 +68,30 @@ def device_info(entry):
         manufacturer=MANUFACTORER,
         model=entry.data.get(CONF_FRIENDLY_NAME, None),
         sw_version=entry.data.get(ATTR_SW_VERSION, None),
-        identifiers={(DOMAIN, entry.data[CONF_MAC])},
-        connections={("mac", entry.data[CONF_MAC])},
+        identifiers={
+            (DOMAIN, entry.data[CONF_MAC])
+        },
+        connections={
+            ("mac", entry.data[CONF_MAC])
+        }
     )
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    _LOGGER.info("Unloading")
-    _LOGGER.info("Entry unloaded")
+    _LOGGER.debug("Unloading")
+    hass.data[DOMAIN][DATA_WORKING] = False
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_unload(entry, component)
+        )
+    hass.data[DOMAIN][DATA_CANCEL]()
+    await hass.async_add_executor_job(hass.data[DOMAIN][entry.entry_id][DATA_CONNECTION].stop)
+    hass.data[DOMAIN][entry.entry_id][DATA_CONNECTION] = None
+    _LOGGER.debug("Entry unloaded")
     return True
-
 
 async def entry_update_listener(hass, entry):
     """Handle options update."""
-    _LOGGER.info("Options updated")
+    cooker = hass.data[DOMAIN][entry.entry_id][DATA_CONNECTION]
+    cooker.persistent = entry.data.get(CONF_PERSISTENT_CONNECTION)
+    _LOGGER.debug("Options updated")
--- custom_components/skycooker/config_flow.py
+++ custom_components/skycooker/config_flow.py
@@ -1,8 +1,23 @@
 """Config flow for SkyCooker integration."""
 
+import logging
+import secrets
+import traceback
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
 from homeassistant import config_entries
+from homeassistant.components import bluetooth
+from homeassistant.const import (CONF_DEVICE, CONF_FRIENDLY_NAME, CONF_MAC,
+                                 CONF_PASSWORD, CONF_SCAN_INTERVAL)
 from homeassistant.core import callback
+
 from .const import *
+from .cooker_connection import CookerConnection
+from .skycooker import SkyCooker
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class SkyCookerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -14,7 +29,127 @@ class SkyCookerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Get options flow for this handler."""
         return SkyCookerConfigFlow(entry=entry)
 
-    def __init__(self, entry=None):
+    def __init__(self, entry = None):
         """Initialize a new SkyCookerConfigFlow."""
         self.entry = entry
         self.config = {} if not entry else dict(entry.data.items())
+
+    async def init_mac(self, mac):
+        mac = mac.upper()
+        mac = mac.replace(':','').replace('-','').replace(' ','')
+        mac = ':'.join([mac[p*2:(p*2)+2] for p in range(6)])
+        id = f"{DOMAIN}-{mac}"
+        if id in self._async_current_ids():
+            return False
+        await self.async_set_unique_id(id)
+        self.config[CONF_MAC] = mac
+        # It's time to create random password
+        self.config[CONF_PASSWORD] = list(secrets.token_bytes(8))
+        return True
+
+    async def async_step_user(self, user_input=None):
+        """Handle the user step."""
+        return await self.async_step_scan()
+   
+    async def async_step_scan(self, user_input=None):
+        """Handle the scan step."""
+        errors = {}
+        if user_input is not None:
+            spl = user_input[CONF_MAC].split(' ', maxsplit=1)
+            mac = spl[0]
+            name = spl[1][1:-1] if len(spl) >= 2 else None
+            if not SkyCooker.get_model_code(name):
+                # Model is not supported
+                return self.async_abort(reason='unknown_model')
+            if not await self.init_mac(mac):
+                # This cooker already configured
+                return self.async_abort(reason='already_configured')
+            if name: self.config[CONF_FRIENDLY_NAME] = name
+            # Continue to connect step
+            return await self.async_step_connect()
+
+        try:
+            try:
+                scanner = bluetooth.async_get_scanner(self.hass)
+                for device in scanner.discovered_devices:
+                    _LOGGER.debug(f"Device found: {device.address} - {device.name}")
+            except:
+                _LOGGER.error("Bluetooth integration not working")
+                return self.async_abort(reason='no_bluetooth')
+            devices_filtered = [device for device in scanner.discovered_devices if device.name and (device.name.startswith("RK-") or device.name.startswith("RFS-"))]
+            if len(devices_filtered) == 0:
+                return self.async_abort(reason='cooker_not_found')
+            mac_list = [f"{r.address} ({r.name})" for r in devices_filtered]
+            schema = vol.Schema(
+            {
+                vol.Required(CONF_MAC): vol.In(mac_list)
+            })
+        except Exception:
+            _LOGGER.error(traceback.format_exc())
+            return self.async_abort(reason='unknown')
+        return self.async_show_form(
+            step_id="scan",
+            errors=errors,
+            data_schema=schema
+        )
+
+    async def async_step_connect(self, user_input=None):
+        """Handle the connect step."""
+        errors = {}
+        if user_input is not None:
+            cooker = CookerConnection(
+                mac=self.config[CONF_MAC],
+                key=self.config[CONF_PASSWORD],
+                persistent=True,
+                adapter=self.config.get(CONF_DEVICE, None),
+                hass=self.hass,
+                model=self.config.get(CONF_FRIENDLY_NAME, None)
+            )
+            tries = 3
+            while tries > 0 and not cooker._last_connect_ok:
+                await cooker.update()
+                tries = tries - 1
+            
+            connect_ok = cooker._last_connect_ok
+            auth_ok = cooker._last_auth_ok
+            cooker.stop()
+        
+            if not connect_ok:
+                errors["base"] = "cant_connect"
+            elif not auth_ok:
+                errors["base"] = "cant_auth"
+            else:
+                return await self.async_step_init()
+
+        return self.async_show_form(
+            step_id="connect",
+            errors=errors,
+            data_schema=vol.Schema({})
+        )  
+
+    async def async_step_init(self, user_input=None):
+        """Handle the options step."""
+        errors = {}
+        if user_input is not None:
+            self.config[CONF_SCAN_INTERVAL] = user_input[CONF_SCAN_INTERVAL]
+            self.config[CONF_PERSISTENT_CONNECTION] = user_input[CONF_PERSISTENT_CONNECTION]
+            fname = f"{self.config.get(CONF_FRIENDLY_NAME, FRIENDLY_NAME)} ({self.config[CONF_MAC]})"
+            # _LOGGER.debug(f"saving config: {self.config}")
+            if self.entry:
+                self.hass.config_entries.async_update_entry(self.entry, data=self.config)
+            _LOGGER.info(f"Config saved")
+            return self.async_create_entry(
+                title=fname, data=self.config if not self.entry else {}
+            )
+
+        schema = vol.Schema(
+        {
+            vol.Required(CONF_PERSISTENT_CONNECTION, default=self.config.get(CONF_PERSISTENT_CONNECTION, DEFAULT_PERSISTENT_CONNECTION)): cv.boolean,
+            vol.Required(CONF_SCAN_INTERVAL, default=self.config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)): vol.All(vol.Coerce(int), vol.Range(min=1, max=60)),
+        })
+
+        return self.async_show_form(
+            step_id="init",
+            errors=errors,
+            data_schema=schema
+        )
